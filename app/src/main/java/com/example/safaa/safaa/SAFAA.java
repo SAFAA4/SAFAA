edf3eab85d4a2c4ae8ad64d181903e25e956cd1e
SAFAA4 <48139185+SAFAA4@users.noreply.github.com>
2019-03-08 17:31:34 +0200

    firts commit

---
+++ app/src/main/java/com/example/safaa/safaa/SAFAA.java
@@ -0,0 +1,13 @@
+package com.example.safaa.safaa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SAFAA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_safa);
+    }
+}
